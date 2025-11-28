a5e292a8a6f5b0a56dcbf6ca98a122dacd56df3d
anandajuliar <anandajulia001@gmail.com>
2025-11-28 19:04:09 +0700

    menambahkan destinasi navigasi menggunakan interface class

--- app/src/main/java/com/example/roomdatabase_046/view/route/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase_046/view/route/DestinasiNavigasi.kt
@@ -1,4 +1,12 @@
 package com.example.roomdatabase_046.view.route
 
 interface DestinasiNavigasi {
+    /**
+     * Nama unik untuk menentukan jalur untuk composable
+     */
+    val route: String
+    /**
+     * String resource id yang berisi judul yang akan ditampilkan di layar
+     */
+    val titleRes: Int
 }
