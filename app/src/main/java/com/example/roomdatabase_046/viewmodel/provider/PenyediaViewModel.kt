3b1e4c45b4b780c5fa08a8b9cbd52a927fc91cc6
anandajuliar <anandajulia001@gmail.com>
2025-11-28 19:48:42 +0700

    membuat penyedia view model pd provider

--- app/src/main/java/com/example/roomdatabase_046/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase_046/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,33 @@
 package com.example.roomdatabase_046.viewmodel.provider
 
+import android.app.Application
+import androidx.lifecycle. ViewModelProvider
+import androidx.lifecycle.viewmodel. CreationExtras
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomdatabase_046.repositori. AplikasiSiswa
+import com.example.roomdatabase_046.viewmodel. EntryViewModel
+import com.example.roomdatabase_046.viewmodel. HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        // Initializer untuk HomeViewModel
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        // Initializer untuk EntryViewModel
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+/**
+ * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah
+ * instance dari [AplikasiSiswa].
+ */
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
