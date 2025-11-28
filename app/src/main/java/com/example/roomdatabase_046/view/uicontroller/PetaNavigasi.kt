4c1c18338e22b61b7021593887da01074a4fb386
anandajuliar <anandajulia001@gmail.com>
2025-11-28 19:45:40 +0700

    menambahkan peta navigasi pd uicontroller

--- app/src/main/java/com/example/roomdatabase_046/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase_046/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,43 @@
 package com.example.roomdatabase_046.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabase_046.view.DestinasiEntry
+import com.example.roomdatabase_046.view.DestinasiHome
+import com.example.roomdatabase_046.view.EntrySiswaScreen
+import com.example.roomdatabase_046.view.HomeScreen
+
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+        // Halaman Home
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+            )
+        }
+
+        // Halaman Entry (Tambah Data)
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+    }
+}
