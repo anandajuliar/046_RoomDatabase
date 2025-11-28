22d6db56f53732bb489ba42404d16949d64ce0a6
anandajuliar <anandajulia001@gmail.com>
2025-11-28 19:05:00 +0700

    menambahkan destinasi entry menggunakan objek

--- app/src/main/java/com/example/roomdatabase_046/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/roomdatabase_046/view/route/DestinasiEntry.kt
@@ -1,4 +1,9 @@
 package com.example.roomdatabase_046.view.route
 
-object DestinasiEntry {
+import com.example.roomdatabase_046.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
+
