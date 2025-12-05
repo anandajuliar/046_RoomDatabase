095ec9b3d7fafcaa398170e21fede319ed9fbac2
anandajuliar <anandajulia001@gmail.com>
2025-12-05 15:54:14 +0700

    memperbaiki objek destinasi detail siswa

--- app/src/main/java/com/example/roomdatabase_046/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_046/view/route/DestinasiDetailSiswa.kt
@@ -2,11 +2,12 @@ package com.example.roomdatabase_046.view.route
 
 import com.example.roomdatabase_046.R
 
-object DestinasiDetailSiswa {
+
+
+
     object DestinasiDetailSiswa : DestinasiNavigasi {
         override val route = "detail_siswa"
         override val titleRes = R.string.detail_siswa
         const val itemIdArg = "idSiswa"
         val routeWithArgs = "$route/{$itemIdArg}"
     }
-}
